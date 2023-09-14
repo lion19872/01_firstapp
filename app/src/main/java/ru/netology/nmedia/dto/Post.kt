223dd924da6267daca5d4d2c1c1028aba9b115a5
Dmitry Levinskiy <lion19872@yandex.ru>
2023-09-14 22:27:08 +0300

    RecyclerView3

---
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -0,0 +1,12 @@
+package ru.netology.nmedia.dto
+
+data class Post(
+    val id: Long,
+    val author: String,
+    val content: String,
+    val published: String,
+    val likes: Int,
+    val likedByMe: Boolean = false,
+    val shares: Int,
+    val views: Int
+)
